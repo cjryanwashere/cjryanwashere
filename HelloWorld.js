74f46214b2cfbdbf2905c9123a8c2497832d0797
cjryanwashere <91854094+cjryanwashere@users.noreply.github.com>
2021-10-06 18:27:59 -0400

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,11 @@
+document.getElementById('file').addEventListener('change', readFile, false);
+
+function readFile(evt) {
+    var files = evt.target.files;
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onload = function(event){
+        console.log(event.target.result);
+    }
+    reader.readAsText(file);
+}
