444abb78960209131ed25c8516bb01308b8b1acd
cjryanwashere <91854094+cjryanwashere@users.noreply.github.com>
2021-10-07 17:15:07 -0400

    Add files via upload

--- uploadclientfiles.js
+++ uploadclientfiles.js
@@ -1,15 +1,39 @@
 //This code has been updated 
+const importObject = {
+  module: {},
+  env: {
+    memory: new WebAssembly.Memory({ initial: 256 }),
+  }
+};
+fetch('main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes =>
+  WebAssembly.instantiate(bytes, importObject)
+).then(results => {
+  const Sum = results.instance.exports.Sum;
+  console.log(Sum(2,3));
+  console.log(Sum(1000, 23));
+});
+
 
 
 
 function gatherTrajFile(evt) {
     var files = evt.target.files[0]; // FileList object
 
+
+    
+
     var reader = new FileReader();
     reader.onload = function(event) {
         console.log(event.target.result);
+        console.log(reader.result)
+        
     }
-    reader.readAsText(files);
+    reader.readAsBinaryString(files);
+    //reader.readAsText(files);
+    
+    
     
   }
 
@@ -26,6 +50,8 @@ function gatherTopFile(evt) {
   }
 
 document.getElementById('trajfiles').addEventListener('change', gatherTrajFile, false);
-
 document.getElementById('topfiles').addEventListener('change', gatherTopFile, false);
+
+// using JP's Sample.wasm to add two numbers 
+
 
